3bc30d7a1b083dbaf5d211f09c74f4c58990acf3
Insoo-Seo <kocon135@gmail.com>
2022-05-12 11:27:00 +0900

    fix: node-sass를 dart-sass로 교체 및 CSS BEM 방법론 적용

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,17 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.s[ac]ss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
       },
     ],
   },
